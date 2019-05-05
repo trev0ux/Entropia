ee8c7b2fbcdb37a58fae10e4c50e732513e2d89b
DrezaVitorio <deza_vitorio@hotmail.com>
2019-05-05 22:46:14 +0000

    Update laboratorio/campNova.php

--- laboratorio/campNova.php
+++ laboratorio/campNova.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<<!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
@@ -10,13 +10,43 @@
         <script type="text/javascript" src="_js/jquery-3.3.1.min.js"></script>
         <script type="text/javascript" src="_js/bootstrap.min.js"></script>
 		<script type="text/javascript" src="js/campanha.js"></script>
-        <link rel="shortcut icon" type="image/x-icon" href="img/logo3.png">
-        <title>ENTROPIA</title>
+        <link rel="shortcut icon" type="image/x-icon" href="img/logo1.png">
+        <title>entropia</title>
 </head>
 <body>
     <?php include'menuSuper.php'?>
-    <div class="interface">
-    
-    </div>
+
+    	<div id="princ_">
+    		<div class="tt"><img src="img/campanha-nova.jpg"></div>
+    		<form method="post" action="">
+    			<p>
+                	<label for="titulo" class="sr-only">Título</label>
+					<input type="text" class="form-control" id="titulo" name="titulo" placeholder="Titulo da Campanha" required>
+               	</p>
+               	<p>
+                	<label for="descricao" class="sr-only">Descrição</label>
+					<textarea type="text" class="form-control" id="descricao" name="descricao" placeholder="Descrição da Campanha" required></textarea>
+               	</p>
+               	<p>
+                	<label for="objetivo" class="sr-only">Objetivo</label>
+					<textarea class="form-control" id="objetivo" name="objetivo" placeholder="Objetivo da Campanha" required></textarea>
+               	</p>
+               	<p>
+                	<label for="regra" class="sr-only">Regras</label>
+					<textarea class="form-control" id="regra" name="regra" placeholder="Regras da Campanha" required></textarea>
+               	</p>
+               	<p>
+                	<label for="premio" class="sr-only">Prêmios</label>
+					<input type="text" class="form-control" id="premio" name="premio" placeholder="Definir premiação para campanha" required>
+               	</p>
+               	<p>
+                	<label for="foto-campanha">Imagem da Campanha</label>
+					<input type="file" class="form-control-file" id="foto-campanha" name="foto-campanha" required>
+               	</p>
+               	<button class="btn btn-primary" type="submit">Salvar</button>
+               	<input type="submit" value="Publicar" class="btn btn-info">
+    		</form>
+    	</div>
+
 </body>
 </html>
